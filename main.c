222db8d46e27a735e01b8af2a91882e0ff695a3c
gabrielaprp <pagnoncelli.gabriela@gmail.com>
2017-04-28 11:02:04 -0300

    if do main

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 // Cauã Antunes e Gabriela Pagnoncelli
 
 int main(int argc, char **argv) {
+	if ( argc == 2 ) {
+		printf( "Argumentos:\n" );
+                for ( i = 1; i < argc; ++i ) {
+                        printf( "  %d. %s\n", i, argv[i] );
+        } else {
+                printf( "No arguments were passed.\n" );
+               }
+        }
 	yyin = fopen(argv[1],"r");
 	initMe();
 	yyparse();
