708ccbbc63c1c5a838e938263db044fe19c45c13
Cauã Antunes <caua.roca@gmail.com>
2017-05-15 09:00:58 -0300

    Mudei as mensagens da main

--- main.c
+++ main.c
@@ -1,16 +1,12 @@
 // Cauã Antunes e Gabriela Pagnoncelli
 
 int main(int argc, char **argv) {
-	if ( argc == 2 ) {
-		printf( "Argumentos:\n" );
-                for ( i = 1; i < argc; ++i ) {
-                        printf( "  %d. %s\n", i, argv[i] );
+	if (argc != 2) {
+               printf( "Invalid number of arguments.\n" );
         } else {
-                printf( "No arguments were passed.\n" );
-               }
-        }
-	yyin = fopen(argv[1],"r");
-	initMe();
-	yyparse();
+		yyin = fopen(argv[1],"r");
+		initMe();
+		yyparse();
+	}
 	return 0;
 }
